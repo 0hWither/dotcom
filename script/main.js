08021541cfeae3bdc11086d7b19689c42c34817f
0hWither <gabrielleeshepherd@gmail.com>
2023-08-18 17:18:13 -0400

    remade site - PASS IS 3.14159

---
+++ script/main.js
@@ -0,0 +1,7 @@
+function openWindow() {
+    var Input = document.getElementById('input')
+        if (Input.value=='3.14159') {
+        Input.value='';
+        var x=window.open("https://codeprojects.org/projects/weblab/Y6sJje41UWAgoAqKm7O-dyEzctOZMGuqJNeFVvAN-Ps");
+        }
+}
