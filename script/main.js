77779c93ffc3d7fdaa8ee936b407af872d5b5ead
0hWither <gabrielleeshepherd@gmail.com>
2023-08-18 20:04:33 -0400

    adding movie files

--- script/main.js
+++ script/main.js
@@ -2,6 +2,6 @@ function openWindow() {
     var Input = document.getElementById('input')
         if (Input.value=='3.14159') {
         Input.value='';
-        var x=window.open("https://codeprojects.org/projects/weblab/Y6sJje41UWAgoAqKm7O-dyEzctOZMGuqJNeFVvAN-Ps");
+        var x=window.open("https://codeprojects.org/projects/weblab/Y6sJje41UWAgoAqKm7O-dyEzctOZMGuqJNeFVvAN-Ps ");
         }
 }
