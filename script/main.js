d82291b396b444e9c03d7115d76e577b8b9b6461
0hWither <gabrielleeshepherd@gmail.com>
2023-08-24 23:10:49 -0400

    added super mario 64

--- script/main.js
+++ script/main.js
@@ -3,5 +3,8 @@ function openWindow() {
         if (Input.value=='3.14159') {
         Input.value='';
         var x=window.open("https://codeprojects.org/projects/weblab/Y6sJje41UWAgoAqKm7O-dyEzctOZMGuqJNeFVvAN-Ps ");
+        } else if (Input.value=='mario64') {
+        Input.value='';
+        var x=window.open("https://0hwither.com/games/super.mario.64/index.html");
         }
 }
