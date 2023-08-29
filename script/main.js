62f215c2c75d00374c1d68c4ffd9411a8db413a5
0hWither <gabrielleeshepherd@gmail.com>
2023-08-29 18:39:53 -0400

    added subway surfers

--- script/main.js
+++ script/main.js
@@ -1,6 +1,6 @@
 function openWindow() {
     var Input = document.getElementById('input')
-        if (Input.value=='3.14159') {
+        if (Input.value=='movies'||Input.value=='3.14159') {
         Input.value='';
         var x=window.open("https://codeprojects.org/projects/weblab/Y6sJje41UWAgoAqKm7O-dyEzctOZMGuqJNeFVvAN-Ps ");
         } else if (Input.value=='mario64') {
@@ -9,5 +9,8 @@ function openWindow() {
         } else if (Input.value=='minecraft') {
             Input.value='';
             var x=window.open("https://0hwither.com/games/minecraft/index.html");
-            }
+        } else if (Input.value=='surfer') {
+            Input.value='';
+            var x=window.open("https://0hwither.com/games/subway.surfers/index.html");
+        }
 }
