a5a7926ae25001e6493bc0064ab6c3846d12b480
0hWither <gabrielleeshepherd@gmail.com>
2023-11-05 14:32:19 -0500

    added rickroll

---
+++ rickroll/script.js
@@ -0,0 +1,32 @@
+console.log("Running rick roll")
+
+document.onkeydown = function(e){
+    e = e || window.event;
+    var key = e.which || e.keyCode;
+    if(key===70||key===27){
+         if(!document.fullscreenElement){
+              requestFullScreen(document.getElementById("rickroll_wrapper"));
+          }else{
+              
+          }
+      }
+};
+
+
+function requestFullScreen(elt) {
+    console.log("Requesting fullscreen for", elt);
+    if (elt.requestFullscreen) {
+        elt.requestFullscreen();
+    } else if (elt.msRequestFullscreen) {
+        elt.msRequestFullscreen();
+    } else if (elt.mozRequestFullScreen) {
+        elt.mozRequestFullScreen();
+    } else if (elt.webkitRequestFullscreen) {
+        elt.webkitRequestFullscreen();
+    } else {
+        console.error("Fullscreen not available");
+    }
+}
+
+let vid = document.getElementById("rickroll");
+vid.volume = 1.0;
