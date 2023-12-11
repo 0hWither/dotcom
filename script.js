5387219982338cef4bed96c7fdd420cd6fb15910
0hWither <gabrielleeshepherd@gmail.com>
2023-12-11 14:20:30 -0500

    remodeling website

---
+++ script.js
@@ -0,0 +1,40 @@
+document.onkeydown = function(e){
+    e = e || window.event;
+    var key = e.which || e.keyCode;
+    if(key===13){
+        check();
+    }
+}
+
+
+var button = document.getElementById("submit")
+var input = document.getElementById("pswd_input")
+
+function check() {
+    if (input.value=="8008135") {
+        correct()
+    } else {
+        wrong()
+    }
+}
+
+function wrong() {
+    console.log("Incorrect PSWD")
+    input.value=""
+    button.className="bt-shake"
+    setTimeout(function(){
+        console.log("stop")
+        button.className="submit_btn"
+    },700)
+}
+
+var main = document.getElementById("main")
+var menu = document.getElementById("menu")
+
+function correct() {
+    /* window.location="games/" */
+    main.className="hide main center"
+    setTimeout(function(){
+        menu.className="main center menu"
+    },600)
+}
--- script/home.js
+++
@@ -1,6 +0,0 @@
-/*LOADING SCREEN*/
-var loader = document.getElementById("preloader")
-window.addEventListener("load",function(){
-    loader.style.display="none"
-});
-
