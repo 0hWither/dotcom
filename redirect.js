af3d06d5d3473bab493a5f69d34b5377379f70f8
0hWither <gabrielleeshepherd@gmail.com>
2024-01-11 19:01:55 -0500

    green

--- redirect.js
+++ redirect.js
@@ -4,4 +4,6 @@ window.onload = function relocate() {
     myWindow.document.write('<html><head><title>Empty Page</title></head><body style="margin:0;">');
     myWindow.document.write('<iframe src="https://takeaim.store/main.html" width=100% height=100% style="margin:0;padding:0;border:none;"></iframe>');
     myWindow.document.write('</body></html>');
+
+    window.open('','_self').close()
 }
