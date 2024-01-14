8ae77d4bb51656294ad6990c4a05af1faefedcfe
0hWither <gabrielleeshepherd@gmail.com>
2024-01-13 21:15:28 -0500

    added cookie clicker and fixed about:blank page

--- redirect.js
+++ redirect.js
@@ -1,7 +1,7 @@
 window.onload = function relocate() {
     console.log("Relocating Webpage");
     var myWindow = window.open();
-    myWindow.document.write('<html><head><link type="image/png" rel="icon" href="https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Google_Drive_icon_%282020%29.svg/2295px-Google_Drive_icon_%282020%29.svg.png" /><title>My Drive - Google Drive</title></head><body style="margin:0;">');
+    myWindow.document.write('<html><head><link type="image/png" rel="icon" href="https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Google_Drive_icon_%282020%29.svg/2295px-Google_Drive_icon_%282020%29.svg.png" /><title>‎ </title></head><body style="margin:0;">');
     myWindow.document.write('<iframe src="https://takeaim.store/main.html" width=100% height=100% style="margin:0;padding:0;border:none;"></iframe>');
     myWindow.document.write('</body></html>');
 
