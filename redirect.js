d49745df1e3971a4f458608eead289e7c018e809
Gabriel <101025354+0hWither@users.noreply.github.com>
2024-02-02 10:51:21 -0500

    Update redirect.js to media.exid.me

--- redirect.js
+++ redirect.js
@@ -3,7 +3,7 @@ window.onload = function relocate() {
     console.log("Relocating Webpage");
     var myWindow = window.open();
     myWindow.document.write('<html><head><link type="image/png" rel="icon" href="https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Google_Drive_icon_%282020%29.svg/2295px-Google_Drive_icon_%282020%29.svg.png" /><title>‎ </title></head><body style="margin:0;">');
-    myWindow.document.write('<iframe src="https://dontbeannoying.shop/main.html" width=100% height=100% style="margin:0;padding:0;border:none;"></iframe>');
+    myWindow.document.write('<iframe src="https://media.exid.me/main" width=100% height=100% style="margin:0;padding:0;border:none;"></iframe>');
     myWindow.document.write('</body></html>');
 
     window.close('','_parent','');
